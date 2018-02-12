ecdd5846de903ab0466dbb1a38fed0ca3c7a5092
Mario Virdis <virdis.mario97@gmail.com>
2018-02-12 22:51:01 +0100

    Aggiunta pagina di ricerca delle richieste.

---
+++ php/search.php
@@ -0,0 +1,39 @@
+<?php
+	if(!isset($_SESSION))
+		session_start();
+
+	require_once __DIR__.'/path.php';
+	include UTILS_DIR.'sessionUtil.php';
+
+	if(!isLogged()) {
+		header('location: ./home.php');
+		exit;
+	}
+
+	if(!isset($_GET['info'])) {
+		$_GET = ['info'=>'toprated'];
+	}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="author" content="Mario Virdis">
+	<meta name="keywords" content="coding code programming social network socialnetwork programs C C++ Java Python">
+	<!-- importo il font Open Sans -->
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300,400">
+	<link rel="stylesheet" href="./../style/friends.css" type="text/css">
+	<link rel="stylesheet" href="./../style/menu.css" type="text/css">
+	<link rel="icon" type="image/png" href="./../images/codeportal_logo2.png"  >
+	<title><?php echo $_SESSION['username']; ?> - CodePortal</title>
+
+	<script type="text/javascript" src="./../scripts/ResponseTable.js"></script>
+	<script type="text/javascript" src="./../scripts/DataWrapper.js"></script>
+	<script type="text/javascript" src="./../scripts/AjaxEngine.js"></script>
+	<script type="text/javascript" src="./../scripts/AjaxActivities.js"></script>
+</head>
+<body>
+	<?php include LAYOUT_DIR.'menu.php'; ?>
+	<h1>In costruzione!</h1>
+</body>
+</html>
