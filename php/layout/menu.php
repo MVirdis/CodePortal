798ac3fff03834d17d09bc25686fd4d23125b08d
Mario Virdis <virdis.mario97@gmail.com>
2018-02-12 22:43:48 +0100

    Esteso il menu nella barra di navigazione con piu' icone.

--- php/layout/menu.php
+++ php/layout/menu.php
@@ -6,7 +6,9 @@
 		<div class="right">
 			<a href="./page.php" class="icon global"></a>
 			<a href="./friends.php" class="icon friends"></a>
+			<a href="./search.php" class="icon search"></a>
 			<a href="./mail.php" class="icon mail"></a>
+			<a href="./profile.php?id=<?php echo $_SESSION['userID']; ?>" class="icon profile"></a>
 			<a href="./utils/logoutUtil.php" class="icon signout"></a>
 			<span class="sx_space default_cursor"><?php echo $_SESSION['username']; ?></span>
 		</div>
