c34e6e4b8fb16ce4e1062adfb369d4df0cab17e8
Mario Virdis <virdis.mario97@gmail.com>
2018-05-05 14:12:54 +0200

    Aggiunge la pagina con le istruzioni sull'utilizzo del sito

---
+++ php/instructions.php
@@ -0,0 +1,22 @@
+<?php
+	if(!isset($_SESSION)) {
+		session_start();
+	}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="author" content="Mario Virdis">
+	<meta name="keywords" content="coding code programming social network socialnetwork programs C C++ Java Python">
+	<title>Code Portal</title>
+	<!-- importo il font Open Sans -->
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300,400">
+
+	<link rel="icon" type="image/png" href="./../images/codeportal_logo2.png"  >
+	<link rel="stylesheet" type="text/css" href="./../style/instructions.css" media="screen">
+</head>
+<body>
+
+</body>
+</html>
